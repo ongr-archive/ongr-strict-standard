47137a0e56e3c811217dd01c67547da49b5dc8cd
Tautrimas Pajarskas <tautrimas.pajarskas@ongr.io>
2014-10-09 15:27:14 +0300

    object separator spacing

--- Sniffs/WhiteSpace/ObjectOperatorSpacingSniff.php
+++ Sniffs/WhiteSpace/ObjectOperatorSpacingSniff.php
@@ -56,11 +56,11 @@ class ONGR_Sniffs_WhiteSpace_ObjectOperatorSpacingSniff implements PHP_CodeSniff
     {
         $tokens = $phpcsFile->getTokens();
 
-        $prevType = $tokens[($stackPtr - 1)]['code'];
-        if (in_array($prevType, PHP_CodeSniffer_Tokens::$emptyTokens) === true) {
-            $error = 'Space found before object operator';
-            $phpcsFile->addError($error, $stackPtr, 'Before');
-        }
+//        $prevType = $tokens[($stackPtr - 1)]['code'];
+//        if (in_array($prevType, PHP_CodeSniffer_Tokens::$emptyTokens) === true) {
+//            $error = 'Space found before object operator';
+//            $phpcsFile->addError($error, $stackPtr, 'Before');
+//        }
 
         $nextType = $tokens[($stackPtr + 1)]['code'];
         if (in_array($nextType, PHP_CodeSniffer_Tokens::$emptyTokens) === true) {
