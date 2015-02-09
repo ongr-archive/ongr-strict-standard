60b4e63dc262a86b2c7d2c469ae135175fbd911f
Erik Maslak <emaslak@nfq.lt>
2015-02-09 10:40:33 +0200

    Added tests for blank line after open tag sniff.

---
+++ Tests/Unit/WhiteSpace/PHPOpenTagSniffTest.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\Tests\Unit\WhiteSpace;
+
+use ONGR\Tests\AbstractSniffUnitTest;
+
+/**
+ * DisallowMultipleAssignmentsSniffTest class.
+ */
+class PHPOpenTagSniffTest extends AbstractSniffUnitTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getErrorList()
+    {
+        return [
+            1 => ['There must be one blank line after the php open tag and no whitespaces'],
+            3 => ['There must be one blank line after the php open tag and no whitespaces'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getWarningList()
+    {
+        return [];
+    }
+}
