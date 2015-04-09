2715936352271acdf73b1a5807a11796697f86bc
David Vondrak <david.vondrak@ongr.io>
2015-04-09 11:06:35 +0300

    Added check of abstract class prefix and trait suffix (ported from previous commit).

---
+++ Sniffs/NamingConventions/TraitSuffixSniff.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * This file is part of the Symfony2-coding-standard (phpcs standard)
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package PHP_CodeSniffer-Symfony2
+ * @author Symfony2-phpcs-authors <Symfony2-coding-standard@escapestudios.github.com>
+ * @license http://spdx.org/licenses/MIT MIT License
+ * @version GIT: master
+ * @link https://github.com/escapestudios/Symfony2-coding-standard
+ */
+
+namespace ONGR\Sniffs\NamingConventions;
+
+use PHP_CodeSniffer_File;
+use PHP_CodeSniffer_Sniff;
+
+/**
+ * Symfony2_Sniffs_NamingConventions_TraitSuffixSniff.
+ *
+ * Throws errors if trait names are not suffixed with "Trait".
+ *
+ * @category PHP
+ * @package PHP_CodeSniffer
+ * @author ONGR Team <info@ongr.io>
+ * @copyright 2015 NFQ Technologies UAB
+ * @license https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+ */
+
+class TraitSuffixSniff implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * @var array A list of tokenizers this sniff supports.
+     */
+    public $supportedTokenizers = [
+        'PHP',
+    ];
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return [T_TRAIT];
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile All the tokens found in the document.
+     * @param int                  $stackPtr  The position of the current token in the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $line = $tokens[$stackPtr]['line'];
+        while ($tokens[$stackPtr]['line'] == $line) {
+            if ('T_STRING' == $tokens[$stackPtr]['type']) {
+                if (substr($tokens[$stackPtr]['content'], -5) != 'Trait') {
+                    $phpcsFile->addError(
+                        'Trait name is not suffixed with "Trait"',
+                        $stackPtr
+                    );
+                }
+                break;
+            }
+            $stackPtr++;
+        }
+    }
+}
---
+++ Tests/Unit/NamingConventions/TraitSuffixSniffTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace ONGR\Tests\Unit\NamingConventions;
+
+use ONGR\Tests\AbstractSniffUnitTest;
+
+/**
+ * TraitSuffixSniffTest class.
+ */
+class TraitSuffixSniffTest extends AbstractSniffUnitTest
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function getErrorList()
+    {
+        return [
+            5 => 1,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getWarningList()
+    {
+        return [];
+    }
+}
