68e46704121e6cb9391da0ad72a30cd7c702b835
Simonas Šerlinskas <simonas.serlinskas@nfq.com>
2015-04-30 13:44:14 +0300

    removed not necessary travis.sh

--- travis.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd ..
-mv $TRAVIS_BUILD_DIR $TRAVIS_BUILD_DIR/../Ongr/
-mkdir $TRAVIS_BUILD_DIR
-cd $TRAVIS_BUILD_DIR/../Ongr/
-git clone https://github.com/ongr-io/ConnectionsBundle.git TmpConnectionsBundle
-cd TmpConnectionsBundle
-git checkout 43df7f8fbaacd47e63d17dc5b2aad6bc10857413
-cd ..
